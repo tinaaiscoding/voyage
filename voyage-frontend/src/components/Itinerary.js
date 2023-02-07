87d4a1ae5bd37b952a35ab822b3425a114aa0c83
tinanvo <tinanhivo@gmail.com>
2023-02-07 21:45:05 +1100

    Fetch weather data for certain city when its clicked in Itinerary Comp.

--- voyage-frontend/src/components/Itinerary.js
+++ voyage-frontend/src/components/Itinerary.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+// import fetchWeather from '../fetch/weather';
 
 const Itinerary = (props) => {
   const [destinationList, setDestinationList] = useState([]);
@@ -6,6 +7,33 @@ const Itinerary = (props) => {
   useEffect(() => {
     setDestinationList(props.destinationList);
   }, [props.destinationList]);
+
+  const fetchWeatherDataHandler = (event) => {
+    const city = event.target.textContent;
+
+    async function getGeoLocation() {
+      const res = await fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=a33a12b5491642ba990f1d8907ae6f5e`
+      );
+      const data = await res.json();
+      const geoLocation = data.results[0].geometry;
+
+      return geoLocation;
+    }
+
+    async function getWeather(latitude, longitude) {
+      const res = await fetch(
+        `https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=2023-01-01&end_date=2023-01-04&daily=temperature_2m_mean,apparent_temperature_mean&timezone=Australia%2FSydney`
+      );
+      const data = await res.json();
+
+      return data;
+    }
+
+    getGeoLocation()
+      .then((geoLocation) => getWeather(geoLocation.lat, geoLocation.lng))
+      .then((weatherData) => console.log(weatherData));
+  };
 
   return (
     <div className="Itinerary">
@@ -15,7 +43,7 @@ const Itinerary = (props) => {
         {destinationList.map(
           (destination, index) =>
             Object.keys(destination).length > 0 && (
-              <p key={index}>
+              <p key={index} onClick={fetchWeatherDataHandler}>
                 {destination.city}, {destination.country}
               </p>
             )
