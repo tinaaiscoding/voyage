5fbe2876592b8467551bdf104447eaaca691ea29
tinanvo <tinanhivo@gmail.com>
2023-02-09 02:06:00 +1100

    Countries list loaded in drop down

--- voyage-frontend/src/components/AddDestination/AddDestinationForm/Countries.js
+++ voyage-frontend/src/components/AddDestination/AddDestinationForm/Countries.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+
+import fetchCountries from '../../../db/countries';
 
 const Countries = (props) => {
+  const [countryList, setCountryList] = useState([]);
+
+  useEffect(() => {
+    fetchCountries().then((res) => {
+      setCountryList(res);
+    });
+  }, []);
+
   const storeCountryHandler = (event) => {
-    const countrySelected = event.target.value
-    
-    props.onSelectedCountry(countrySelected)
-  }
+    const countrySelected = event.target.value;
+
+    props.onSelectedCountry(countrySelected);
+  };
 
   return (
     <div className="Countries">
@@ -15,11 +25,21 @@ const Countries = (props) => {
         onChange={storeCountryHandler}
         value={props.selectedCountry}
       >
-        <option>COUNTRY</option>
+        {countryList.map((country, index) => {
+          return (
+            <option key={index} value={country.name}>
+              {country.name}
+            </option>
+          );
+        })}
+
+        {/* <option>COUNTRY</option>
         <option value="Australia">Australia</option>
         <option value="United Kingdom">United Kingdom</option>
-        <option value="United States of America">United States of America</option>
-        <option value="Brazil">Brazil</option>
+        <option value="United States of America">
+          United States of America
+        </option>
+        <option value="Brazil">Brazil</option> */}
       </select>
     </div>
   );
---
+++ voyage-frontend/src/db/countries.js
@@ -0,0 +1,21 @@
+const fetchCountries = async () => {
+  var headers = new Headers();
+  headers.append(
+    'X-CSCAPI-KEY',
+    'SkpFbGZOaFhBTTZINnpTWU15TzdSYk5FMHZaMFVjVU5PMng2UHNIdw=='
+  );
+
+  var requestOptions = {
+    method: 'GET',
+    headers: headers,
+    redirect: 'follow',
+  };
+
+  const res = await fetch('https://api.countrystatecity.in/v1/countries', requestOptions)
+  const countriesList = await res.json();
+  const countries = countriesList
+
+  return countries;
+};
+
+export default fetchCountries;
